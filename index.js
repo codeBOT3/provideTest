86a44e8c7be600bf8aa0161a69e5edc2e0fc4d34
ThemDankMeme <duvalvdv@gmail.com>
2023-09-05 15:26:15 +0200

    changed comparison for date change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function compareDates(defaultStart, defaultEnd, target){
     let file = await getRelativeJson(target);
 
     for(let [startDateB, endDateB] of file.dates ){
-        console.log('testing file', startDateB, endDateB)
+        console.log('\ntesting file', startDateB, endDateB)
         if(defaultStart <= endDateB && defaultEnd >= startDateB){
             if(defaultStart>=startDateB)
                 startDateB = defaultStart;
@@ -28,5 +28,5 @@ async function compareSets(file_A, file_B){
 }
 
 let list = ['./dates1.json', './dates2.json', './dates3.json'];
-await compareDates('2024/01/01', '2024/12/31', './dates1.json');
-await compareDates('2024/01/01', '2024/12/31', './dates2.json');
+await compareDates('2024/01/01', '2024/12/31', './set_a.json');
+await compareDates('2024/01/01', '2024/12/31', './set_b.json');
