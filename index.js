4971110b05a3c59bacabf35a0b74d29a4cd5941d
ThemDankMeme <duvalvdv@gmail.com>
2023-09-05 13:31:44 +0200

    default start & end date added

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 import {getRelativeJson} from "./sourceJSON.js";
 
-async function compareDates(startDateA, endDateA, target){
+async function compareDates(defaultStart, defaultEnd, target){
     let file = await getRelativeJson(target);
-    console.log(file);
+
     for(let [startDateB, endDateB] of file.dates ){
         console.log('testing file', startDateB, endDateB)
-        if(startDateA <= endDateB && endDateA >= startDateB){
-            console.log('overlap');
+        if(defaultStart <= endDateB && defaultEnd >= startDateB){
+            if(defaultStart<=startDateB)
+                startDateB = defaultStart;
+            if(defaultEnd<=endDateB)
+                endDateB = defaultEnd;
+            console.log(`testing date change`, startDateB, endDateB);
+
         }
     }
 
 
 }
+async function compareSets(file_A, file_B){
+    const startDate = '2024/01/01';
+    const endDate = '2024/12/31';
 
-await compareDates('2024/01/01', '2024/12/31', './dates.json');
+    let setA = await getRelativeJson(file_A);
+    let setB = await getRelativeJson(file_B);
+
+
+}
+
+let list = ['./dates1.json', './dates2.json', './dates3.json'];
+await compareDates('2024/01/01', '2024/12/31', './dates1.json');
+await compareDates('2024/01/01', '2024/12/31', './dates2.json');
